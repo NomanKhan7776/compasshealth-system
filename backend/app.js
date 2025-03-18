957c1ce30a095bfb0dca10df0c6df19ced048b60
nomankhan7776 <nknomankhan30@gmail.com>
2025-03-18 07:03:48 +0500

    cors updated

--- backend/app.js
+++ backend/app.js
@@ -18,11 +18,11 @@ const app = express();
 
 // Middleware
 // app.use(express.json());
+app.use(cors());
 app.use("/api/auth", express.json());
 app.use("/api/users", express.json());
 app.use("/api/assignments", express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
